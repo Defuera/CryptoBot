34242d94133c79c172ca0253f3500fd54546d2f0
vs <vasily.shc@gmail.com>
2017-10-02 00:21:19 +0300

    store token in keystore, access it vis build config

--- src/main/kotlin/ru/justd/cryptobot/Main.kt
+++ src/main/kotlin/ru/justd/cryptobot/Main.kt
@@ -10,7 +10,7 @@ import com.pengrad.telegrambot.request.SendMessage
 import com.pengrad.telegrambot.response.SendResponse
 import java.io.IOException
 
-val BOT_TOKEN = "" //todo store in local gradle file
+val BOT_TOKEN = BuildConfig.BOT_TOKEN
 val bot = TelegramBotAdapter.build(BOT_TOKEN)
 
 fun main(args: Array<String>) {
@@ -34,7 +34,7 @@ private fun processUpdate(update: Update) {
     println("message ${message?.entities()?.get(0)?.type() ?: ""}: ${message?.text() ?: "null"}")
 
     val entities = message.entities()
-    if (entities?.isNotEmpty() ?: false) { //todo is there's a better way
+    if (entities?.isNotEmpty() == true) { //todo is there's a better way
         entities.forEach {
             when (it.type()) {
                 bot_command -> handleBotCommand(message)
