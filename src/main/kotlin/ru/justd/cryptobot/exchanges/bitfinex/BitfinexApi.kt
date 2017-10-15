a7ed3f918b597f9ae617a63eec5b28d21937359c
defuera <vdenis@zvooq.com>
2017-10-15 22:45:59 +0200

    refactor exchanges with PollingExchange abstract class

---
+++ src/main/kotlin/ru/justd/cryptobot/exchanges/bitfinex/BitfinexApi.kt
@@ -0,0 +1,38 @@
+package ru.justd.cryptobot.exchanges.bitfinex
+
+import okhttp3.OkHttpClient
+import ru.justd.cryptobot.exchanges.PollingExchange
+import ru.justd.cryptobot.exchanges.RateResponse
+import ru.justd.cryptobot.exchanges.exceptions.RequestFailed
+
+private const val BASE_URL = "https://api.bitfinex.com/v1"
+
+/**
+ * https://docs.bitfinex.com/v1/reference
+ */
+class BitfinexApi(okHttpClient: OkHttpClient) : PollingExchange(okHttpClient) {
+
+    companion object {
+        const val NAME = "BITFINEX"
+    }
+
+    override fun getRateUrl(base: String, target: String) = "$BASE_URL/pubticker/$base$target"
+
+    @Throws(RequestFailed::class)
+    override fun parseResponseBody(bodyString: String, base: String, target: String): RateResponse {
+        val ticker = gson.fromJson<Ticker>(bodyString, Ticker::class.java)
+        return RateResponse(ticker.mid, base, target)
+    }
+
+    private data class Ticker(
+            val mid: Double,
+            val bid: Double,
+            val ask: Double,
+            val last_price: Double,
+            val low: Double,
+            val high: Double,
+            val volume: Double,
+            val timestamp: Double
+    )
+
+}
