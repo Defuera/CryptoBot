9eb9a974cd1231f176d387bbc42cc8ccf83e90cb
defuera <vdenis@zvooq.com>
2017-10-22 18:29:03 +0200

    clean up, add docs

--- core/src/main/kotlin/ru/justd/cryptobot/handler/InstantFactory.kt
+++ core/src/main/kotlin/ru/justd/cryptobot/handler/InstantFactory.kt
@@ -1,5 +1,9 @@
 package ru.justd.cryptobot.handler
 
+/**
+ * Factory for simple handlers, which not require runtime configuration.
+ * Usually those handlers are singletons (or [object](https://kotlinlang.org/docs/reference/object-declarations.html#object-declarations) in kotlin)
+ */
 class InstantFactory<out T : CommandHandler> constructor(scheme : String, private val commandHandler: T) : CommandHandlerFactory<T>(scheme) {
 
     override fun create(channelId: String, request: String): T = commandHandler
