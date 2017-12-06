1344370f620b1d103549b4609c084c950deada12
defuera <vdenis@zvooq.com>
2017-12-06 09:31:17 +0100

    fix start text

--- telegram/src/main/kotlin/ru/justd/cryptobot/handler/StartHandler.kt
+++ telegram/src/main/kotlin/ru/justd/cryptobot/handler/StartHandler.kt
@@ -12,13 +12,13 @@ class StartHandler(private val analytics: Analytics) : CommandHandler {
 
         analytics.trackStart(channelId)
         return Reply(channelId,
-                """
-                    ${BuildConfig.BOT_NAME} is your buddy in the cryptocurrencies world.
-                    It helps you be up to date with popular coins prices. You can fetch prices via `/price` command or `/subscribe` for updates.
-                    To see full list of commands just start typing `/`.
+"""
+  ${BuildConfig.BOT_NAME} is your buddy in the cryptocurrencies world.
+It helps you be up to date with popular coins prices. You can fetch prices via `/price` command or `/subscribe` for updates.
+To see full list of commands just start typing `/`.
 
-                    ${BuildConfig.BOT_NAME} is an open source software.
-                    """
+  ${BuildConfig.BOT_NAME} is an open source software.
+"""
                         .trimMargin()
         )
     }
